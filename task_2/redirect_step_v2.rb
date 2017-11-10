ac7c35ac207e422ca9f0195be502d1baa82c8b2a
Dnar <Chanky@list.ru>
2017-11-11 01:57:44 +0300

    Update redirect_step_v2.rb

--- task_2/redirect_step_v2.rb
+++ task_2/redirect_step_v2.rb
@@ -1,10 +1,10 @@
 require 'net/http'
 require 'uri'
 
-uri = URI.parse("https://onetwotrip.com")
+uri = URI.parse("https://__.com")
 response = Net::HTTP.get_response(uri)
 
-if response.get_fields('Location').eql?(["https://www.onetwotrip.com/"]) && response.code.to_i == 301
+if response.get_fields('Location').eql?(["https://www.__.com/"]) && response.code.to_i == 301
   puts "ОК"
 else
   puts response.get_fields('Location')
